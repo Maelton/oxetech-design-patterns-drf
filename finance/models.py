67130b9a7b4432b5d952fc449d8d0a03f5e640db
Maelton Lima <maelton.main@gmail.com>
2025-05-26 16:52:59 -0300

    feat: add SupplierCategory CRUD

--- finance/models.py
+++ finance/models.py
@@ -8,7 +8,7 @@ class Supplier(models.Model):
     phone = models.CharField("Phone", max_length=20, blank=True)
     address = models.TextField("Address")
     contact_person = models.CharField("Contact Person", max_length=255, blank=True)
-    # category -> Future relationship
+    category = models.ForeignKey('SupplierCategory', on_delete=models.PROTECT)
     is_active = models.BooleanField("Active", default=True)
     created_at = models.DateTimeField("Created At", auto_now_add=True)
     updated_at = models.DateTimeField("Updated At", auto_now=True)
@@ -16,6 +16,18 @@ class Supplier(models.Model):
     class Meta:
         verbose_name = "Supplier"
         verbose_name_plural = "Suppliers"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class SupplierCategory(models.Model):
+    name = models.CharField("Name", max_length=100, unique=True)
+    description = models.TextField("Description", blank=True)
+
+    class Meta:
+        verbose_name = "Supplier Category"
+        verbose_name_plural = "Supplier Categories"
         ordering = ["name"]
 
     def __str__(self):
--- finance/serializers.py
+++ finance/serializers.py
@@ -1,8 +1,14 @@
-from .models import Supplier
+from .models import Supplier, SupplierCategory
 from rest_framework import serializers
 
 class SupplierSerializer(serializers.ModelSerializer):
         
     class Meta:
         model = Supplier
+        fields = '__all__'
+
+class SupplierCategorySerializer(serializers.ModelSerializer):
+        
+    class Meta:
+        model = SupplierCategory
         fields = '__all__'
