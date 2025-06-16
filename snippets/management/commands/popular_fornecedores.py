687320afb88c2b5e1d54ec1c2b5aa04e37e06aea
Maelton Lima <maelton.main@gmail.com>
2025-06-16 15:08:16 -0300

    feat: add popular_fornecedores command

--- snippets/management/commands/popular_fornecedores.py
+++ snippets/management/commands/popular_fornecedores.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from snippets.models.fornecedor import Fornecedor
+from snippets.models.fornecedor import Fornecedor, Cidade
 from snippets.tasks import send_email
 import xlrd
 
@@ -21,8 +21,28 @@ class Command(BaseCommand):
         
         # print(objeto, _)
         
-        book = xlrd.open_workbook("./fornecedores.xls")
+        suppliers = [];
         
-        send_email.delay("test@gmail.com")
+        book = xlrd.open_workbook("./fornecedores.xls");
+        sheet = book.sheet_by_index(0);
+        
+        for row in range(1, sheet.nrows):    
+            name = sheet.cell_value(row, 0);
+            tax_id = str(sheet.cell_value(row, 1))[:-2];
+            city = sheet.cell_value(row, 2);
+            street = sheet.cell_value(row, 3);
+            
+            if not Fornecedor.objects.filter(nome=name, cnpj=tax_id).exists():
+                suppliers.append(
+                    Fornecedor(
+                        nome=name,
+                        cnpj=tax_id,
+                        cidade=Cidade.objects.filter(city).first(),
+                        logradouro=street
+                    )
+                );
+        
+        Fornecedor.objects.bulk_create(suppliers);
+        send_email.delay("test@gmail.com");
         
         
